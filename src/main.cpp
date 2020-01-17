687c7c7b68150b4685b3730cdafe427625743ee4
sillyghost <racker4g@gmail.com>
2020-01-17 13:25:43 -0500

    Changing core params

--- src/main.cpp
+++ src/main.cpp
@@ -1726,7 +1726,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    if (nHeight == 1) return COIN * 200000000;
+    CAmount nSubsidy = 1.5 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
