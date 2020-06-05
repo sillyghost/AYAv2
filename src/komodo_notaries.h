1b8fc4dfe83e8c38c79cd121048f19919e1e2559
Silly Ghost <34451068+sillyghost@users.noreply.github.com>
2020-06-05 23:14:20 +0530

    Change season 4 activation height

--- src/komodo_notaries.h
+++ src/komodo_notaries.h
@@ -23,7 +23,7 @@
  // first season had no third party coins, so it ends at block 0. 
  // second season ends at approx block 2430952, Please check this it should be as close as possible to July 15th 0:00 UTC!!!!!!!
  // third season ending height is unknown so it set to very very far in future. 
- static const int32_t KMD_SEASON_HEIGHTS[NUM_KMD_SEASONS] = {0, 335000,400500 ,999999999};
+ static const int32_t KMD_SEASON_HEIGHTS[NUM_KMD_SEASONS] = {0, 335000,399500 ,999999999};
 
  // Era array of pubkeys. Add extra seasons to bottom as requried, after adding appropriate info above. 
  static const char *notaries_elected[NUM_KMD_SEASONS][NUM_KMD_NOTARIES][2] =
