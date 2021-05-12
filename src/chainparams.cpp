6d94525b75c306ea1dd0bf03ecc2221bd7ee1bcf
sillyghost <rack3r@protonmail.com>
2021-05-12 18:58:03 -0400

    Increase Compatibility with older versions

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,7 +120,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("54.39.99.180");
+        vSeeds.emplace_back("dnsseed1.aryacoin.io");
 
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,23);
